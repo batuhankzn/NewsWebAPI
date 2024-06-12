bb5f333aa690cfd1b228c0c50dff7a8b1cb087be
batuhankzn <230119091@student.adiguzel.edu.tr>
2024-06-12 14:52:03 +0300

    endpoints added

--- NewsWebAPI/Controller/NewsController.cs
+++ NewsWebAPI/Controller/NewsController.cs
@@ -5,30 +5,94 @@ using System.Threading.Tasks;
 
 namespace NewsWebAPI.Controllers
 {
- [ApiController]
-[Route("api/[controller]")]
-public class NewsController : ControllerBase
-{
-    private readonly NewsWebDbContext _context;
-
-    public NewsController(NewsWebDbContext context)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NewsController : ControllerBase
     {
-        _context = context;
+        private readonly NewsWebDbContext _context;
+
+        public NewsController(NewsWebDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<News>>> GetNews()
+        {
+            return await _context.News.ToListAsync();
+        }
+
+        [HttpGet("GetNews/{id}")]
+        public async Task<ActionResult<News>> GetNews(int id)
+        {
+            var news = await _context.News.FindAsync(id);
+
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return news;
+        }
+
+        [HttpPost("AddNews")]
+        public async Task<ActionResult<News>> AddNews(News news)
+        {
+            _context.News.Add(news);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNews", new { id = news.Id }, news);
+        }
+
+        [HttpDelete("DeleteNews/{id}")]
+        public async Task<IActionResult> DeleteNews(int id)
+        {
+            var news = await _context.News.FindAsync(id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            _context.News.Remove(news);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("UpdateNews/{id}")]
+        public async Task<IActionResult> UpdateNews(int id, News news)
+        {
+            if (id != news.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(news).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NewsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool NewsExists(int id)
+        {
+            return _context.News.Any(e => e.Id == id);
+        }
     }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<News>>> GetNews()
-    {
-        return await _context.News.ToListAsync();
-    }
-
-    [HttpPost("AddNews")]
-    public async Task<ActionResult<News>> AddNews(News news)
-    {
-        _context.News.Add(news);
-        await _context.SaveChangesAsync();
-
-        return CreatedAtAction("GetNews", new { id = news.Id }, news);
-    }
-}
+    
 }
