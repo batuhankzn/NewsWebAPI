42c368c1589b77bf005b397c1a3581c60abd1802
batuhankzn <230119091@student.adiguzel.edu.tr>
2024-06-15 00:11:29 +0300

    updated database

--- NewsWebAPI/Entities/News.cs
+++ NewsWebAPI/Entities/News.cs
@@ -6,6 +6,9 @@ public class News
     public required string Title { get; set; }
     public required string Content { get; set; }
     public DateTime Date { get; set; }
+    public string Image { get; set; }
+    public string Category { get; set; }
+    public string Author { get; set; }
 
 
 }
